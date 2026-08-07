febfcbcd40fc82be68386e57d59630a2a7e94bc2
abudyha123 <abudyhamawy23@gmail.com>
2026-08-07 10:27:32 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        glow: '0 8px 25px rgba(234, 179, 8, 0.45)',
+      },
+    },
   },
   plugins: [],
 };
